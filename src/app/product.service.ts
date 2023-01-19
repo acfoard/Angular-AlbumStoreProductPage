77520ef95480924535846f42558051b6220c935d
Andrew Foard <Andrew Foard@hq.gbhinc.com>
2023-01-19 10:07:26 -0500

    3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@Angular/http';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map'
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+import { Album } from './album';
+
+import { Observable } from 'rxjs/Observable';
+
+//import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
+  private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
+
   constructor(private _http: Http) { }
 
-  private _albumUrl: string = '../assets/album.json';
-
-  getAlbum(id: number): Observable<any> {
-   return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
+
+  // getProducts(): Observable<Product[]> {
+  //   return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  // }
 }
